d52f3ba9ee8201d6d9ea8a45f13eecbeb174374b
SimplyFaisal <d.f.animashaun@gmail.com>
2014-09-20 03:46:14 -0400

    angular directives

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,15 @@
 var app = angular.module("jogaB",[]);
 
 app.controller("clubDropDown",["$scope","$http",function($scope,$http){
-      $scope.selected = "";
-      $scope.years = [];
+      $scope.selected = "germany";
+      $scope.year = "2000"
+      var data = [];
+      for (var i = 2000; i < 2014; i++) {
+        var name = i +" - " + (i + 1);
+        var value = ""+ i;
+        data.push({"name":name,"value":value});
+      }
+      $scope.seasons = data;
       $scope.leagues = [
         {name:"Bundesliga",value:"germany"},
         {name:"Ligue 1",value:"france"},
@@ -10,7 +17,11 @@ app.controller("clubDropDown",["$scope","$http",function($scope,$http){
         {name:"Premier League",value:"england"},
         {name:"Seria A",value:"italy"},
       ];
-      $http.get('/'+ $scope.selected + "@').success(function(data){
-        $scope.news = data;
-      });
+      $scope.response = function() {
+        var route = '/'+ $scope.selected + '@' + $scope.year;
+        $http.get(route).success(function(data){
+          $scope.news = data;
+          console.log(route)
+        });
+      }
     }]);
