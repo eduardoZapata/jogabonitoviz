399524d669bac22eb855a3b6e894d1f4321629f1
SimplyFaisal <d.f.animashaun@gmail.com>
2014-09-19 22:39:24 -0400

    init

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+var monk = require('monk');
+//var db = require('monk')('mongodb://joga:United99@dbh55.mongolab.com:27557/heroku_app25040865');
+var db = require('monk')('localhost/jogaB');
+
+app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
